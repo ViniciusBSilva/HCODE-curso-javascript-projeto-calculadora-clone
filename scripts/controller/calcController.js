de0ac915680df4653e005f9787d8de2061fa482a
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2021-11-11 21:12:19 -0300

    attributes, getters and setters

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,32 @@
-class CalcController{
-    
+class CalcController {
+
+    constructor() {
+
+        // "this" means that this variable is an attribute for this class
+
+        // "_" at the begining of the name of the variable is a convention to say 
+        // that the attribute is private although it has no functionality to effectively
+        // block the access to the attribute
+
+        this._displayCalc = "0";
+        this._dataAtual;
+
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor) {
+        this._displayCalc = valor;
+    }
+
+    get dataAtual() {
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor) {
+        this._dataAtual = valor;
+    }
+
 }
